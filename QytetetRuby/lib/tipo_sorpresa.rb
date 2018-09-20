18dcfd76257b013ee78611cc1663e1dcbf37e934
noelia <noelia@noelia-PC>
2018-09-20 21:35:32 +0200

    TipoSorpresa

---
+++ QytetetRuby/lib/tipo_sorpresa.rb
@@ -0,0 +1,11 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+module TipoSorpresa
+    PAGARCOBRAR = :Pagar_cobrar
+    IRACASILLA =:Ir_A_Casilla
+    PORCASAHOTEL =:Por_Casa_Hotel
+    PORJUGADOR =:Por_Jugador
+    SALIRCARCEL =:Salir_Carcel
+end
