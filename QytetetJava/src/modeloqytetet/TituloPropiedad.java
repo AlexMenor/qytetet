a3dacf9223f951c312c20cf046ee975ae10c2fc5
AlexMenor <alexmenor@gmail.com>
2018-09-26 12:07:51 +0200

    titulo propiedad

--- QytetetJava/src/modeloqytetet/TituloPropiedad.java
+++ QytetetJava/src/modeloqytetet/TituloPropiedad.java
@@ -15,7 +15,7 @@ public class TituloPropiedad {
         // Nombre de la calle
     private String nombre;
         // ¿Está hipotecada?
-    private boolean hipotecada;
+    private boolean hipotecada = false;
         // Su precio
     private int precioCompra;
         // Precio base por caer en la casilla
@@ -24,15 +24,77 @@ public class TituloPropiedad {
            Revalorización de la propiedad entre
            su compra y venta 
         */
-    private float factorRevalorización;
+    private float factorRevalorizacion;
         // Valor base de su hipoteca
     private int hipotecaBase;
         // Precio de edificación
     private int precioEdificar;
         // Número de casas edificadas
-    private int numCasa;
+    private int numCasas = 0;
         // Número de hoteles edificados
-    private int numHoteles;
+    private int numHoteles = 0;
+    
+        // Constructor
+    public TituloPropiedad
+        (String nombre, int precioCompra, int alquilerBase,
+         int factorRevalorizacion, int hipotecaBase, 
+         int precioEdificar)
+        {
+            this.nombre = nombre;
+            this.precioCompra = precioCompra;
+            this.alquilerBase = alquilerBase;
+            this.factorRevalorizacion = factorRevalorizacion;
+            this.hipotecaBase = hipotecaBase;
+            this.precioEdificar = precioEdificar;
+        }
+        
+        // Consultores
+
+    public String getNombre() {return nombre;}
+
+    public boolean isHipotecada() {return hipotecada;}
+
+    public int getPrecioCompra() {return precioCompra;}
+
+    public int getAlquilerBase() {return alquilerBase;}
+
+    public float getFactorRevalorizacion() {
+        return factorRevalorizacion;
+    }
+    
+    public int getHipotecaBase() {return hipotecaBase;}
+
+    public int getPrecioEdificar() {return precioEdificar;}
+
+    public int getNumCasas() {return numCasas;}
+
+    public int getNumHoteles() {return numHoteles;}
+    
+        // Modificador hipotecada
+    
+    public void setHipotecada (boolean hipotecada){
+        this.hipotecada = hipotecada;
+    }
+    
+        // TituloPropiedad toString()
+
+    @Override
+    public String toString() {
+        return "TituloPropiedad{" + "nombre=" + nombre 
+                + ", hipotecada=" + hipotecada 
+                + ", precioCompra=" + precioCompra 
+                + ", alquilerBase=" + alquilerBase 
+                + ", factorRevalorizacion=" + factorRevalorizacion 
+                + ", hipotecaBase=" + hipotecaBase 
+                + ", precioEdificar=" + precioEdificar 
+                + ", numCasas=" + numCasas 
+                + ", numHoteles=" + numHoteles + '}';
+    }
+    
+    
+    
+        
+    
     
     
 }
