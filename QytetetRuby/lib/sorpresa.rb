5fc611040470c3b757e1e08a15715be761ad54bf
noelia <noelia@noelia-PC>
2018-09-26 18:21:45 +0200

    Comentarios y otras modificaciones

--- QytetetRuby/lib/sorpresa.rb
+++ QytetetRuby/lib/sorpresa.rb
@@ -10,11 +10,15 @@ module ModeloQytetet
     attr_accessor:valor
     attr_accessor:tipo
 
+    # Constructor que crea una carta sorpesa a partir de 3 argumentos
+    
     def initialize (texto, valor, tipo)
       @texto = texto
       @valor = valor
       @tipo = tipo
     end
+    
+    # Método que imprime los atributos de una carta sorpresa
 
     def to_s()
       "Texto; #{@texto} \n Valor: #{@valor} \n Tipo: #{@tipo}"
