e392fa51fed72754f9602c5e68339d6573161008
escaleranm <escaleranm@ei142106>
2018-09-26 12:06:11 +0200

    tablero

---
+++ QytetetJava/src/modeloqytetet/Tablero.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeloqytetet;
+import java.util.ArrayList;
+/**
+ *
+ * @author escaleranm
+ */
+public class Tablero {
+    private ArrayList<Casilla> casillas = new ArrayList<>();
+    private Casilla carcel;
+    
+    public Tablero(){
+    }
+    
+}
