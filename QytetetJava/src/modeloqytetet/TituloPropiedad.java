13706093e4d2cdd597f308934fb0114e49ceef76
AlexMenor <alexmenor@gmail.com>
2018-09-26 11:49:33 +0200

    tipo casilla y titulo propiedad creados

---
+++ QytetetJava/src/modeloqytetet/TituloPropiedad.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeloqytetet;
+
+/**
+ *
+ * @author alex
+ */
+public class TituloPropiedad {
+    // Campos privados asociados a la propiedad
+    
+        // Nombre de la calle
+    private String nombre;
+        // ¿Está hipotecada?
+    private boolean hipotecada;
+        // Su precio
+    private int precioCompra;
+        // Precio base por caer en la casilla
+    private int alquilerBase;
+        /*
+           Revalorización de la propiedad entre
+           su compra y venta 
+        */
+    private float factorRevalorización;
+        // Valor base de su hipoteca
+    private int hipotecaBase;
+        // Precio de edificación
+    private int precioEdificar;
+        // Número de casas edificadas
+    private int numCasa;
+        // Número de hoteles edificados
+    private int numHoteles;
+    
+    
+}
