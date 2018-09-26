dcaf22d1680de742fb1099f8b790ace3cfb2ee2b
escaleranm <escaleranm@ei142106>
2018-09-26 12:32:42 +0200

    mas tablero

--- QytetetJava/src/modeloqytetet/Tablero.java
+++ QytetetJava/src/modeloqytetet/Tablero.java
@@ -10,10 +10,31 @@ import java.util.ArrayList;
  * @author escaleranm
  */
 public class Tablero {
-    private ArrayList<Casilla> casillas = new ArrayList<>();
+    private ArrayList<Casilla> casillas;
     private Casilla carcel;
     
+    private void inicializar(){
+        casillas = new ArrayList<>();
+        
+        casillas.add (new Casilla (1, 300, new TituloPropiedad ("Huerto de "
+                + "Calixto y Melibea", 300, 50, (float)1.2, 150, 250)));
+    }
+
+    
     public Tablero(){
+        carcel = new Casilla(9, TipoCasilla.CARCEL);
+    }
+    
+    public ArrayList<Casilla> getCasillas(){
+        return casillas;
+    }
+    
+    public Casilla getCarcel(){
+        return carcel;
+    }
+    
+    public String toString(){
+        return "Casillas";
     }
     
 }
