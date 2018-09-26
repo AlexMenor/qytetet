1a283f4959873e6576b89983a27b476c1b624376
escaleranm <escaleranm@ei142106>
2018-09-26 13:05:30 +0200

    Más tablero

--- QytetetJava/src/modeloqytetet/Tablero.java
+++ QytetetJava/src/modeloqytetet/Tablero.java
@@ -16,8 +16,38 @@ public class Tablero {
     private void inicializar(){
         casillas = new ArrayList<>();
         
-        casillas.add (new Casilla (1, 300, new TituloPropiedad ("Huerto de "
-                + "Calixto y Melibea", 300, 50, (float)1.2, 150, 250)));
+        casillas.add (new Casilla (0, TipoCasilla.SALIDA));
+        casillas.add (new Casilla (1, new TituloPropiedad ("Huerto de "
+                + "Calixto y Melibea", 200, 50, (float)1.2, 150, 250)));
+        casillas.add (new Casilla (2, new TituloPropiedad ("Avda de los"
+                + " cipreses", 350, 55, (float)1.3, 200, 255)));
+        casillas.add (new Casilla (3, new TituloPropiedad ("Avda Carmen"
+                + " Martín Gaite", 400, 60, (float)1.4, 300, 300)));
+        casillas.add (new Casilla (4, TipoCasilla.PARKING));
+        casillas.add (new Casilla (5, new TituloPropiedad ("Calle Cuarta", 
+                425, 62, (float)1.4, 400, 325)));
+        casillas.add (new Casilla (6, new TituloPropiedad ("Avda Vicente del"
+                + "bosque", 460, 67, (float)1.43, 450, 350)));
+        casillas.add (new Casilla (7, TipoCasilla.SORPRESA));
+        casillas.add (new Casilla (8, new TituloPropiedad ("Rana de la "
+                + "Universidad", 500, 70, (float)1.5, 475, 400)));
+        casillas.add (new Casilla (9, TipoCasilla.CARCEL));
+        casillas.add (new Casilla (10, new TituloPropiedad ("Palacio de "
+                + "Monterrey", 600, 75, (float)1.6, 500, 450)));
+        casillas.add (new Casilla (11, new TituloPropiedad ("Plaza de "
+                + "Barcelona", 750, 80, (float)1.65, 600, 475)));
+        casillas.add (new Casilla (12, TipoCasilla.JUEZ));
+        casillas.add (new Casilla (13, new TituloPropiedad ("Ieronimus", 800,
+                85, (float)1.7, 700, 500)));
+        casillas.add (new Casilla (14, TipoCasilla.SORPRESA));
+        casillas.add (new Casilla (15, new TituloPropiedad ("Puente romano",
+                900, 90, (float)1.75, 850, 550)));
+        casillas.add (new Casilla (16, new TituloPropiedad ("Catedral de "
+                + "Salamanca", 1000, 95, (float)1.9, 900, 600)));
+        casillas.add (new Casilla (17, TipoCasilla.IMPUESTO));
+        casillas.add (new Casilla (18, TipoCasilla.SORPRESA));
+        casillas.add (new Casilla (19, new TituloPropiedad ("Gran Vía", 1500,
+                100, (float)1.95, 1000, 700)));
     }
 
     
