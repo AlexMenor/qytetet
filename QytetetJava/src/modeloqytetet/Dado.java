a9fca937316efc53cca5d886836dd19fb6a3b6a2
Arelaxe <36896793+Arelaxe@users.noreply.github.com>
2018-10-08 21:21:44 +0200

    Cambios menores en comentarios etc

--- QytetetJava/src/modeloqytetet/Dado.java
+++ QytetetJava/src/modeloqytetet/Dado.java
@@ -1,25 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modeloqytetet;
-
-/**
- *
- * @author escaleranm
- */
+/* 
+     Esta clase representa
+     el dado del juego
+    */
 class Dado {
-    private int valor;
+    // Atributos privados    
+    private int valor; // Valor del dado al tirarlo
+        
+    // Métodos privados
+    private static final Dado instance = new Dado (); // Método dado que crea
+                                                      // la única instancia de dado
+ 
+    private Dado () {valor = 1;} // Consturctor de la instancia única del dado
+                                 // Valor 1 por defecto
     
-    private static final Dado instance = new Dado ();
+    // Métodos públicos 
+    public static Dado getInstance () {return instance;} // Devuelve la instancia
+                                                         // única del dado
     
-    private Dado () {valor = 1;} // Valor 1 por defecto
+    public int getValor () {return valor;} // Consultor: devuelve el valor actual
+                                           // del dado
     
-    public static Dado getInstance () {return instance;}
-    
-    public int getValor () {return valor;}
-    
+    // Método toString, que devuelve una cadena con los campos del dado
     @Override
     public String toString (){
         return "Dado{Valor: " + valor+ "}";
