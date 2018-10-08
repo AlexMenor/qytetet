15bc8ff027d70c4375843e9465b1b67da6ae02c4
Arelaxe <36896793+Arelaxe@users.noreply.github.com>
2018-10-08 21:21:31 +0200

    Cambios menores en comentarios etc

--- QytetetJava/src/modeloqytetet/MetodoSalirCarcel.java
+++ QytetetJava/src/modeloqytetet/MetodoSalirCarcel.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package modeloqytetet;
 
-/**
- *
- * @author escaleranm
- */
+package modeloqytetet;
+/*
+    Dato de tipo enumerado que representa las distintas maneras de salir
+    de la cárcel
+*/
 public enum MetodoSalirCarcel {
     TIRANDODADO, PAGANDOLIBERTAD 
 }
