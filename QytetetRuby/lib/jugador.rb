c5c2dd36a8c8dfd8f1637a35e78901d231509d38
noelia <noelia@noelia-HP-Laptop-15-bw0xx>
2018-10-23 21:32:21 +0200

    Mi parte

--- QytetetRuby/lib/jugador.rb
+++ QytetetRuby/lib/jugador.rb
@@ -18,6 +18,79 @@ class Jugador
     @propiedades = Array.new
   end
   
+  def <=> (otroJugador)
+    otroJugador.obtener_capital <=> obtener_capital
+  end
+  
+  def obtener_capital
+    to_return = @saldo
+    
+    @propiedades.each do |propiedad|
+      precio_propiedad = propiedad.precio_compra
+      
+      numero_edificios = propiedad.num_casas + propiedad.num_hoteles
+      
+      precio_propiedad += numero_edificios * propiedad.precio_edificar
+      
+      if (propiedad.hipotecada)
+        precio_propiedad -= propiedad.hipoteca_base
+      end
+      
+      to_return += precio_propiedad
+    end
+    return to_return
+  end
+  
+  def cuantas_casa_hoteles_tengo
+    to_return = 0
+    
+    @propiedades.each do |propiedad|
+      to_return += propiedad.num_casas + propiedad.num_hoteles
+    end
+    
+    return to_return
+  end
+  
+  def devolver_carta_libertad
+    tmp = @carta_libertad
+    @carta_libertad = nil
+    
+    return tmp
+  end
+  
+  def pagar_impuesto
+    @saldo -= @casilla_actual.coste
+  end
+  
+  def tengo_carta_libertad
+    return !@carta_libertad.nil?
+  end
+  
+  def es_de_mi_propiedad (titulo)
+    return @propiedades.include?(titulo)
+  end
+  
+  def modificar_saldo (cantidad)
+    @saldo += cantidad
+    
+    return @saldo
+  end
+  
+  def obtener_propiedades (estado_hipotecada)
+    to_return = Array.new
+    
+    @propiedades.each do |propiedad|
+      if (propiedad.hipotecada == estado_hipotecada)
+        to_return << propiedad
+      end
+    end
+    return to_return
+  end
+  
+  def tengo_saldo (cantidad)
+    return (@saldo > cantidad)
+  end
+  
   # toString()
   
   def to_s
@@ -34,6 +107,9 @@ class Jugador
     if (!propiedades.nil?)
       to_return << ("\nPropiedades: #{@propiedades.to_s}")
     end
+    
+    to_return += "\nCapital: "
+    to_return += obtener_capital.to_s
     
     to_return
   end
