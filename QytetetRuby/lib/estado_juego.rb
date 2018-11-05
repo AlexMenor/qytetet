97d6b28c19e5782017e2a06e0b6fa54df903aab4
noelia <noelia@noelia-HP-ENVY-17-Notebook-PC>
2018-11-05 18:40:35 +0100

    Ruby

---
+++ QytetetRuby/lib/estado_juego.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+module ModeloQytetet
+  
+  module EstadoJuego
+    
+    JA_CONSORPRESA = :ja_consorpresa 
+    ALGUNJUGADORENBANCARROTA = :algunjugadorenbancarrota 
+    JA_PUEDECOMPRAROGESTIONAR = :ja_puedecomprargestionar
+    JA_PUEDEGESTIONAR = :ja_puedegestionar 
+    JA_PREPARADO = :ja_preparado 
+    JA_ENCARCELADO = :ja_encarcelado 
+    JA_ENCARCELADOCONOPCIONDELIBERTAD = :ja_encarceladoconopciondelibertad
+   
+  end
+end
