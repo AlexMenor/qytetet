658cf11298d6eb1c2f16ac1524615411c059977c
noelia <noelia@noelia-HP-ENVY-17-Notebook-PC>
2018-11-11 12:58:06 +0100

    Comentarios

--- QytetetJava/src/modeloqytetet/TipoSorpresa.java
+++ QytetetJava/src/modeloqytetet/TipoSorpresa.java
@@ -1,5 +1,9 @@
 package modeloqytetet;
 
+/*
+    Dato de tipo enumerado que representa los diferentes tipos de sorpresa
+*/
+
 public enum TipoSorpresa {
     PAGARCOBRAR,IRACASILLA,PORCASAHOTEL,PORJUGADOR,SALIRCARCEL
 }
