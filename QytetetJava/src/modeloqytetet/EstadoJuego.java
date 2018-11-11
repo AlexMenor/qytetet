59a6ec92b3541526281cdf1d0775f076d279f2ef
noelia <noelia@noelia-HP-ENVY-17-Notebook-PC>
2018-11-11 12:57:23 +0100

    Comentarios

--- QytetetJava/src/modeloqytetet/EstadoJuego.java
+++ QytetetJava/src/modeloqytetet/EstadoJuego.java
@@ -1,6 +1,10 @@
 
 package modeloqytetet;
 
+/*
+    Dato de tipo enumerado que representa los estados que puede
+    tomar el juego
+*/
 
 public enum EstadoJuego {
     JA_CONSORPRESA, ALGUNJUGADORENBANCARROTA, JA_PUEDECOMPRAROGESTIONAR,
