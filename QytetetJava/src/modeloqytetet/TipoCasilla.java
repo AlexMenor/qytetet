0625fb156d1ad75e012bc076b39c0f2d6566385b
noelia <noelia@noelia-HP-ENVY-17-Notebook-PC>
2018-11-11 12:57:56 +0100

    Comentarios

--- QytetetJava/src/modeloqytetet/TipoCasilla.java
+++ QytetetJava/src/modeloqytetet/TipoCasilla.java
@@ -1,5 +1,9 @@
 package modeloqytetet;
 
+/*
+    Dato de tipo enumerado que representa los distintos tipos de casillas
+*/
+
 public enum TipoCasilla {
     SALIDA, CALLE, SORPRESA, CARCEL, JUEZ, IMPUESTO, PARKING
 }
