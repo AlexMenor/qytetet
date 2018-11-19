d64e865c416d52c3ed157c3e7e4003ded45cfc6d
noelia <noelia@noelia-HP-ENVY-17-Notebook-PC>
2018-11-19 21:58:50 +0100

    Mi parte de java

--- QytetetJava/src/modeloqytetet/Especulador.java
+++ QytetetJava/src/modeloqytetet/Especulador.java
@@ -5,45 +5,69 @@ package modeloqytetet;
 public class Especulador extends Jugador {
     private int fianza;
     
-    // Método para pagar impuesto 
     
-    protected void pagarImpuesto (int cantidad){
+    // Constructor de especulador
+    protected Especulador (Jugador jugador, int fianza){
+        super (jugador);
+        this.fianza = fianza;
+    }
+    
+    // Método para pagar impuesto 
+    @Override
+    protected void pagarImpuesto (){
+        modificarSaldo (-(getCasillaActual().getCoste()/2));
+    }
+    
+    // Método para consultar si el especulador debe ir a la cárcel
+    
+    @Override
+    protected boolean deboIrACarcel(){
+        return super.deboIrACarcel() && !pagarFianza();
+    }
+    
+    // Método privado para pagar la fianza
+    
+    private boolean pagarFianza(){
+        boolean pagada = false;
+        
+        if (getSaldo() > fianza){
+            modificarSaldo(-fianza);
+            pagada = true;
+        }
+        return pagada;
     }
     
     // Método para obtener el factor de especulador
 //    @Override
 //    protected int getFactorEspeculador(){}
     
-    // Constructor de especulador
-    
-    protected Especulador (Jugador jugador, int fianza){
-        super (jugador);
-        this.fianza = fianza;
-    }
-    
     // Método para convertirse
     @Override
     protected Especulador convertirme (int fianza){
         return this;
     }
+
+    // Método que sobreescribe el de jugador y devuelve si puede edificar una casa
     
-//    // Método para consultar si el especulador debe ir a la cárcel
-//    
-//    @Override
-//    protected boolean deboIrACarcel(){}
-//    
-//    // Método privado para pagar la fianza
-//    
-//    private boolean pagarFianza(){}
-//    
-//    // Método que sobreescribe el de jugador y devuelve si puede edificar una casa
-//    
-//    @Override
-//    protected boolean puedoEdificarCasa(){}
-//    
-//    // Método que sobreescribe el de jugador y devuelve si puede edificar una casa
-//    
-//    @Override
-//    protected boolean puedoEdificarHotel(){}
+    @Override
+    protected boolean puedoEdificarCasa(TituloPropiedad titulo){
+        return (titulo.getNumCasas() < 8 && tengoSaldo(titulo.getPrecioEdificar()));
+    }
+    
+    // Método que sobreescribe el de jugador y devuelve si puede edificar una casa
+    
+    @Override
+    protected boolean puedoEdificarHotel(TituloPropiedad titulo){
+        return (titulo.getNumHoteles() < 8 && tengoSaldo(titulo.getPrecioEdificar()) && titulo.getNumCasas() == 4);
+    }
+    
+    @Override
+    public String toString(){
+        String aDevolver = super.toString();
+        aDevolver += "\nFianza: ";
+        aDevolver += fianza;
+        
+        return aDevolver;
+    }
     
 }
