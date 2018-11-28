f41d880b44fbad95175a720373b92af1bba29e73
escaleranm <escaleranm@ei140097>
2018-11-28 12:26:24 +0100

    Prueba de especulador

--- QytetetRuby/lib/prueba_qytetet.rb
+++ QytetetRuby/lib/prueba_qytetet.rb
@@ -18,6 +18,7 @@ require_relative "casilla"
 require_relative "titulo_propiedad"
 require_relative "calle"
 require_relative "otra_casilla"
+require_relative "especulador"
 
 module ModeloQytetet
   
