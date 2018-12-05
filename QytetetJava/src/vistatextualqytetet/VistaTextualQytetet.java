3736f6f8bb65a689d4f990b45876e9bf7b42c6b4
escaleranm <escaleranm@ei140097>
2018-12-05 12:36:10 +0100

    Apartado D en java

---
+++ QytetetJava/src/vistatextualqytetet/VistaTextualQytetet.java
@@ -0,0 +1,22 @@
+
+
+package vistatextualqytetet;
+
+import java.util.ArrayList;
+import controladorqytetet.*;
+
+public class VistaTextualQytetet {
+    // Atributos privados
+    private ControladorQytetet controlador;
+    private modeloqytetet.Qytetet modelo;
+    
+    // Métodos públicos
+    
+    //public ArrayList<String> obtenerNombreJugadores(){}
+    
+    //public int elegirCasilla(int opcionMenu){}
+    
+    //public String leerValorCorrecto(ArrayList<String> valoresCorrectos){}
+    
+    //public int elegirOperacion(){}
+}
