d2e9c5c9ae2d8caaeb9b04485efb980db7a79f9e
AlexMenor <alex4menor@gmail.com>
2018-12-11 18:32:25 +0100

    F 1)

--- QytetetRuby/lib/ControladorQytetet/controlador_qytetet.rb
+++ QytetetRuby/lib/ControladorQytetet/controlador_qytetet.rb
@@ -16,9 +16,53 @@ module ControladorQytetet
       @modelo = Qytetet.new
       @nombre_jugadores = Array.new
     end
-    def obtener_casillas_validas (opcion)
+    def obtener_casillas_validas ()
+      array_de_opciones = Array.new
+      
+      if @modelo.jugadores.empty?
+        array_de_opciones.push(OpcionMenu.index(INICIARJUEGO))
+        return array_de_opciones
+      end
+      
+      array_de_opciones.push(OpcionMenu.index(TERMINARJUEGO))
+      array_de_opciones.push(OpcionMenu.index(MOSTRARJUGADORACTUAL))
+      array_de_opciones.push(OpcionMenu.index(MOSTRARJUGADORES))
+      array_de_opciones.push(OpcionMenu.index(MOSTRARTABLERO))
+      
+      estado = @modelo.estado_juego
+      
+      case estado
+      when ModeloQytetet::EstadoJuego::JA_PREPARADO
+        array_de_opciones.push(OpcionMenu.index(JUGAR))
+      when ModeloQytetet::EstadoJuego::JA_PUEDECOMPRAROGESTIONAR
+        array_de_opciones.push(OpcionMenu.index(PASARTURNO))
+        array_de_opciones.push(OpcionMenu.index(COMPRARTITULOPROPIEDAD))
+        array_de_opciones.push(OpcionMenu.index(VENDERPROPIEDAD))
+        array_de_opciones.push(OpcionMenu.index(HIPOTECARPROPIEDAD))
+        array_de_opciones.push(OpcionMenu.index(CANCELARHIPOTECA))
+        array_de_opciones.push(OpcionMenu.index(EDIFICARCASA))
+        array_de_opciones.push(OpcionMenu.index(EDIFICARHOTEL))
+       when ModeloQytetet::EstadoJuego::JA_PUEDEGESTIONAR
+        array_de_opciones.push(OpcionMenu.index(PASARTURNO))
+        array_de_opciones.push(OpcionMenu.index(VENDERPROPIEDAD))
+        array_de_opciones.push(OpcionMenu.index(HIPOTECARPROPIEDAD))
+        array_de_opciones.push(OpcionMenu.index(CANCELARHIPOTECA))
+        array_de_opciones.push(OpcionMenu.index(EDIFICARCASA))
+        array_de_opciones.push(OpcionMenu.index(EDIFICARHOTEL))
+      when ModeloQytetet::EstadoJuego::JA_CONSORPRESA
+        array_de_opciones.push(OpcionMenu.index(APLICARSORPRESA))
+      when ModeloQytetet::EstadoJuego::ALGUNJUGADORENBANCARROTA
+        array_de_opciones.push(OpcionMenu.index(OBTENERRANKING))
+      when ModeloQytetet::EstadoJuego::JA_ENCARCELADO
+        array_de_opciones.push(OpcionMenu.index(PASARTURNO))
+      when ModeloQytetet::EstadoJuego::JA_ENCARCELADOCONOPCIONDELIBERTAD
+        array_de_opciones.push(OpcionMenu.index(INTENTARSALIRCARCELTIRANDODADO))
+        array_de_opciones.push(OpcionMenu.index(INTENTARSALIRCARCELPAGANDOLIBERTAD))
+      end
+      
+      return array_de_opciones
     
-    end
+    end 
     
     def necesita_elegir_casilla (opcion)
     
