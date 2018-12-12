058ace757bd9777da55e9c5b6bac1839ca203531
escaleranm <escaleranm@ei142097>
2018-12-12 12:43:48 +0100

    Punto 2 ruby

--- QytetetRuby/lib/ControladorQytetet/controlador_qytetet.rb
+++ QytetetRuby/lib/ControladorQytetet/controlador_qytetet.rb
@@ -28,6 +28,7 @@ module ControladorQytetet
       array_de_opciones.push(OpcionMenu.index(MOSTRARJUGADORACTUAL))
       array_de_opciones.push(OpcionMenu.index(MOSTRARJUGADORES))
       array_de_opciones.push(OpcionMenu.index(MOSTRARTABLERO))
+      array_de_opciones.push(OpcionMenu.index(OBTENERRANKING))
       
       estado = @modelo.estado_juego
       
@@ -51,8 +52,6 @@ module ControladorQytetet
         array_de_opciones.push(OpcionMenu.index(EDIFICARHOTEL))
       when ModeloQytetet::EstadoJuego::JA_CONSORPRESA
         array_de_opciones.push(OpcionMenu.index(APLICARSORPRESA))
-      when ModeloQytetet::EstadoJuego::ALGUNJUGADORENBANCARROTA
-        array_de_opciones.push(OpcionMenu.index(OBTENERRANKING))
       when ModeloQytetet::EstadoJuego::JA_ENCARCELADO
         array_de_opciones.push(OpcionMenu.index(PASARTURNO))
       when ModeloQytetet::EstadoJuego::JA_ENCARCELADOCONOPCIONDELIBERTAD
@@ -73,7 +72,96 @@ module ControladorQytetet
     end
     
     def realizar_operacion(opcion, casilla)
+      a_realizar = OpcionMenu[opcion]
       
+      case a_realizar
+      when INICIARJUEGO
+        @modelo.inicializar_juego(@nombre_jugadores)
+        to_return = "\nIniciamos el juego con los jugadores #{@nombre_jugadores}"
+      when JUGAR
+        @modelo.jugar
+        to_return = "\nDado: #{@modelo.valor} Casilla: #{@modelo.casilla_actual}"
+      when APLICARSORPRESA
+        to_return = "\nLa sorpresa es #{@modelo.carta_actual}"
+        @modelo.aplicar_sorpresa
+      when INTENTARSALIRCARCELPAGANDOLIBERTAD
+        encarcelado = @modelo.intentar_salir_carcel(ModeloQytetet::MetodoSalirCarcel::PAGANDOLIBERTAD)
+        if (encarcelado)
+          to_return = "\nNo se ha salido de la cárcel"
+        else
+          to_return = "\nSe ha salido de la cárcel"
+        end
+      when INTENTARSALIDCARCELTIRANDODADO
+        encarcelado = @modelo.intentar_salir_carcel(ModeloQytetet::MetodoSalirCarcel::TIRANDODADO)
+        if (encarcelado)
+          to_return = "\nNo se ha salido de la cárcel"
+        else
+          to_return = "\nSe ha salido de la cárcel"
+        end
+      when COMPRARTITULOPROPIEDAD
+        comprado = @modelo.comprar_titulo_propiedad
+        if (!comprado)
+          to_return = "\nNo se ha comprado la propiedad #{@modelo.casilla_actual}"
+        else
+          to_return = "\nSe ha comprado la propiedad #{@modelo.casilla_actual}"
+        end
+      when HIPOTECARPROPIEDAD
+        hipotecada = @modelo.hipotecar_propiedad(casilla)
+        if (hipotecada)
+          to_return = "\nSe ha hipotecado la propiedad #{casilla}"
+        else
+          to_return = "\nNo se ha hipotecado la propiedad #{casilla}"
+        end
+      when CANCELARHIPOTECA
+        cancelada = @modelo.cancelar_hipoteca(casilla)
+        
+        if (hipotecada)
+          to_return = "\nSe ha cancelado la hipoteca de la propiedad #{casilla}"
+        else
+          to_return = "\nNo se ha cancelado la hipoteca de la propiedad #{casilla}"
+        end
+      when EDIFICARCASA
+        edificada = @modelo.edificar_casa(casilla)
+        
+        if (edificada)
+          to_return = "\nSe ha edificado una casa en la propiedad #{casilla}"
+        else
+          to_return = "\nNo se ha edificado una casa en la propiedad #{casilla}"
+        end
+      when EDIFICARHOTEL
+        edificada = @modelo.edificar_hotel(casilla)
+        
+        if (edificada)
+          to_return = "\nSe ha edificado un hotel en la propiedad #{casilla}"
+        else
+          to_return = "\nNo se ha edificado un hotel en la propiedad #{casilla}"
+        end
+      when VENDERPROPIEDAD
+        vendida = @modelo.vender_propiedad(casilla)
+        
+        if (vendida)
+          to_return = "\nSe ha vendido la propiedad #{casilla}"
+        else
+          to_return = "\nNo se ha vendido la propiedad #{casilla}"
+        end
+      when PASARTURNO
+        @modelo.siguiente_jugador
+        to_return = "\nLe toca a #{@modelo.jugador_actual}"
+      when OBTENERRANKING
+        @modelo.obtener_ranking
+        to_return = "\nLos jugadores ordenados: #{@modelo.jugadores}"
+      when TERMINARJUEGO
+        @modelo.obtener_ranking
+        to_return = "\nJuego terminado. Ranking: #{@modelo.jugadores}"
+      when MOSTRARJUGADORACTUAL
+        to_return = "#{@modelo.jugador_actual}"
+      when MOSTRARJUGADORES
+        to_return = "#{@modelo.jugadores}"
+      when MOSTRARTABLERO
+        to_return = "#{@modelo.tablero}"
+      end
+
+      return to_return
     end
   end
 end
